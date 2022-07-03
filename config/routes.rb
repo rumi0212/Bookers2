27cb484d1a5a44ff69a3e4525c616ad28d227f0a
rumi0212 <106750897+rumi0212@users.noreply.github.com>
2022-07-03 08:42:39 +0000

    [Add]aboutページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root :to => 'homes#top'
   resources :books
   devise_for :users
+  get "homes/about" => "homes#about", as: "about"
 
   resources :books, only: [:new, :create, :index, :show]
   resources :users, only: [:show, :edit]
