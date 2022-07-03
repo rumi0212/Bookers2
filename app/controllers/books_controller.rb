f63b2dcdf1d30229d9362e69d6bee943ebb80ed5
rumi0212 <106750897+rumi0212@users.noreply.github.com>
2022-07-03 05:47:34 +0000

    [Add]users_controllerのファイルの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,13 @@ class BooksController < ApplicationController
      @book = Book.new
   end
 
+  def create
+    @book =Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path
+  end
+
   def index
   end
 
@@ -11,4 +18,11 @@ class BooksController < ApplicationController
 
   def edit
   end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
