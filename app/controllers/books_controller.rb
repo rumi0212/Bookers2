992f509b52e05ec66cef591c446f57784a86a5b0
rumi0212 <106750897+rumi0212@users.noreply.github.com>
2022-07-18 16:17:53 +0000

    [Add]bookのcreate機能の作成,bookのshowページの作成,bookのeditページの作成,bookのupdate機能の作成,bookのdestroy機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+
   def new
      @book = Book.new
   end
@@ -32,10 +33,30 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render "edit"
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to "/books"
+  end
+
   private
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+  
+  def user_params
+    params.require(:user).permit(:name,:profile_image,:introduction)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   get "homes/about" => "homes#about", as: "about"
 
-  resources :books, only: [:new, :create, :index, :show]
+  resources :books
   resources :users, only: [:index, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
